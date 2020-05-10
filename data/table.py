f2748a79d67bef2352035daa1c7258356da831a1
Vladimir <vladimiralekxeev@yandex.ru>
2020-05-10 11:50:21 +0300

    Add database net

--- data/table.py
+++ data/table.py
@@ -13,9 +13,6 @@ class User(SqlAlchemyBase):
     password_salt = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
     public_key = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=False)
 
-    def __str__(self):
-        return f"User({self.login}, {self.password})"
-
 
 class Token(SqlAlchemyBase):
     __tablename__ = "tokens"
@@ -34,8 +31,8 @@ class UsedEmail(SqlAlchemyBase):
 class Chat(SqlAlchemyBase):
     __tablename__ = "chats"
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    user1 = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
-    user2 = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
+    user1 = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey('users.username'), unique=False, nullable=False)
+    user2 = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey('users.username'), unique=False, nullable=False)
     chat_id = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
     last_update = sqlalchemy.Column(sqlalchemy.Float, unique=False, nullable=False, default=time.time)
 
@@ -47,9 +44,11 @@ class Message(SqlAlchemyBase):
     data = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
     signature = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
     unix_time = sqlalchemy.Column(sqlalchemy.Float, unique=False, nullable=False)
-    chat_id = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
+    chat_id = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey('chats.chat_id'), unique=False,
+                                nullable=False)
     viewed = sqlalchemy.Column(sqlalchemy.Boolean, unique=False, nullable=False)
-    sent_by = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
+    sent_by = sqlalchemy.Column(sqlalchemy.String, sqlalchemy.ForeignKey('users.username'), unique=False,
+                                nullable=False)
     keys = sqlalchemy.Column(sqlalchemy.String, unique=False, nullable=False)
 
 
